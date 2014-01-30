e8004484e721c68d0511ccb704e0306738de5e2a
joalmay <joalmay@gmail.com>
2014-01-29 19:29:22 -0700

    Step2
    Signed-off-by: joalmay <joalmay@gmail.com>

--- FriendController.js
+++ FriendController.js
@@ -1,6 +1,20 @@
-app.controller("FriendController", function($scope){
-//	$scope.text = "Hello World!";
-console.log("My Ctrl is working!");
+app.controller("FriendController", function($scope, $http){
+	
+	$http.get("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json")
+		.success(function(data) {
+
+			//console.log("friends", friends);	
+			$scope.friends=data.results;
+			
+			});
+
+	//$scope.name = "Hello World!";
+	//console.log("My Ctrl is working!");
+	//$scope.urlpic = "https://scontent-a.xx.fbcdn.net/hprofile-ash3/833_10200838803828124_1233160321_a.jpg";
+     //$scope.location = "Status: I hate the snow. I wish I was on the beach right now!!! ";
+
+
 
 });
+
 
